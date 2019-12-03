3aba07459cba774b13fd007539ad4eedecc983f4
Andy Tran <aktran321@gmail.com>
2019-12-03 14:21:57 -0500

    entered in the forms

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -3,21 +3,19 @@ import React, { Component } from 'react';
 class SignUp extends Component {
     render() {
         return (
-            <div class ="container">
-    <div class="row">
-        <div className='col-10 mx-auto text-center text-title text-uppercase pt-5'>
-            <form action="/sign_up" method="post">
+            <div className ="container">
+    <div className="row">
+        <div className='col-10 mx-auto text-center text-title pt-5'>
+            <form action="/post-feedback" method="post">
                 <h1> Sign Up Form</h1>
-                <input class="box" type="text" name="name" id="name" placeholder="Name" required/>
+                <input className="box" type="text" name="firstName" id="firstName" placeholder="First Name" required/>
                 <br></br>
-
-                <input class="box" type="email" name="email" id="email"
+                <input className="box" type="text" name="lastName" id="lastName" placeholder="Last Name" required/>
+                <br></br>
+                <input className="box" type="email" name="email" id="email"
                 placeholder="E-mail" required/> <br></br>
-
-                <input class="box" type="text" name="phone" id="phone"
-                placeholder="Phone Number" required/> <br></br>
                 
-                <input class="box" type="password" name="password"
+                <input className="box" type="password" name="password"
                 id="password" placeholder="Password" required/> 
                 <br></br>
                 <br></br>
@@ -25,6 +23,7 @@ class SignUp extends Component {
                 <input type="submit" id = "submitDetails"
                 name="submitDetails" value="Submit"/> <br></br>
             </form>
+            <a href="view-feedbacks">View Feedbacks</a>
         </div>
     </div>
 </div>
